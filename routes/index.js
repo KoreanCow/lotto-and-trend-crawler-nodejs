22b40a7a87d87ff92f0bfce1e84ecbb771084084
KoreanCow <tyunnon1257@naver.com>
2019-12-19 20:07:44 +0900

    basic

--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,57 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/naver', (req, res, next) => {
+router.get('/naver', (req, res, next) => {
   request('https://www.naver.com/', (error, response, body) => {
-    // console.log(response)// 객체로 저장 
-    // console.log(body) html 문서
-
     const $ = cheerio.load(body);
     let SilGum = [];
 
-    
-    // #PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_list.PM_CL_realtimeKeyword_list_base > ul:nth-child(1) > li:nth-child(1) > a > span.ah_k
-    // div.ah_list > ul.ah_l > li.ah_item > span.ah_k
-    let ulList = [];
     let strarr = $("div.ah_list ul.ah_l").children("li.ah_item");
-
     strarr.each(function (index, item) {
       SilGum[index] = {
-        num: index+1,
+        num: index,
         title: $(this).find('span.ah_k').text()
       }; 
+      
+    });
+    console.log(SilGum);
+    res.render('naver', {title: SilGum} );
+    // res.render('naver', {title: SilGum[1].title} );
+    
   });
-  res.json(SilGum);
-  });
+})
+router.post('/lotto', (req, res, next) => {
+  // let week;
+  request('https://www.dhlottery.co.kr/common.do?method=main', (req, res, body) => {
+    const $ = cheerio.load(body);
+
+    let week = $("#lottoDrwNo").text();
+    week = Number(week);
+    console.log(week);
+    
+    // for(let i = 0; i <= 5; i++){
+      // let i = 5;
+      // week = week;
+      request(`https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=${week}`, (req, res, body) => {
+      let $ = cheerio.load(body);
+
+      let Choiced_Number = [];
+      let intarr = $("div.num.win").children("p");
+      intarr.each( function (index, item) {
+        Choiced_Number[index] = {
+          week:  week,
+          number : Number($(this).find('span').text())
+        }
+        console.log(Choiced_Number[0].number);
+      })
+      });
+  })
+  
 })
 
-router.post('/rotto', (req, res, next) => {
-  request('')
-})
+
 module.exports = router;
+
+  // #lottoDrwNo
+  // https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=825 825회 로또 페이지
+  // drwNo= "int" 사용
